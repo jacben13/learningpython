d95b50acafbb56d8bb8b119dd4488f144867a901
Ben Jacobs <jacben13@gmail.com>
2014-09-07 22:31:00 -0700

    fixed typo in shuffle method for Deck

--- blackjack.py
+++ blackjack.py
@@ -1,20 +1,23 @@
 __author__ = 'Ben'
 import random
 
+
 class Deck(object):
     cards = []
     cardsremaining = 0
+
     def __init__(self, decks):
         self.shuffle(decks)
 
     def shuffle(self, decks):
-        for n in range(1,14):
-            self.cards.append(n)
-            self.cards.append(n)
-            self.cards.append(n)
-            self.cards.append(n)
-            self.cardsremaining += 4
-        self.cardscards = random.shuffle(self.cards)
+        for d in range(decks):
+            for n in range(1, 14):
+                self.cards.append(n)
+                self.cards.append(n)
+                self.cards.append(n)
+                self.cards.append(n)
+                self.cardsremaining += 4
+        self.cards = random.shuffle(self.cards)
 
     def dealonecard(self):
         card = self.cards[0]
@@ -24,17 +27,20 @@ class Deck(object):
 
     def printdeck(self):
         print(self.cards)
-        print("Cards remaining: "+str(self.cardsremaining))
+        print("Cards remaining: " + str(self.cardsremaining))
+
 
 class Player(object):
     # players will have hands, which will be passed card/values
     # players will also have money, strategies, and the option to be player or computer controlled
     pass
 
+
 def mainloop():
-    deckshoe = Deck(1)
+    deckshoe = Deck(2)
     deckshoe.printdeck()
     deckshoe.dealonecard()
     deckshoe.printdeck()
+
 
 mainloop()
