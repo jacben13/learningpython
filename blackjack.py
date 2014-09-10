d03791c869c451badd314af19136a98df590c8b3
Ben Jacobs <jacben13@gmail.com>
2014-09-09 20:35:54 -0700

    basic play functionality implemented, dealer moves automated.

--- blackjack.py
+++ blackjack.py
@@ -44,6 +44,7 @@ class Player(object):
 
     money = 0
     hand = []
+    blackjack = False
     card_values = {"acelow": 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 10, 12: 10, 13: 10,
                    "acehigh": 11}
     card_names = {1: 'Ace', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10', 11: 'Jack',
@@ -52,6 +53,8 @@ class Player(object):
     def __init__(self, money):
         self.money = money
         self.hand = []
+        self.bust = False
+        self.blackjack = False
 
     def get_card(self, d):
         # assert d is isinstance(object, Deck)
@@ -59,6 +62,7 @@ class Player(object):
 
     def clear_hand(self):
         self.hand = []
+        self.bust = False
 
     def get_total(self):
         total = 0
@@ -88,7 +92,9 @@ class Player(object):
         return total
 
     def bust_or_not(self):
-        return self.get_total() > 21
+        if self.get_total() > 21:
+            self.bust() = True
+        return self.bust
 
     def return_status_string(self, player_number):
         s = ""
@@ -108,11 +114,40 @@ class Player(object):
             s += str(self.get_total()) + "\n"
         return s
 
+    def dealer_moves(self, d):
+        if self.bust:
+            return 1
+        while not self.bust or self.get_total() < 17:
+            self.get_card(d)
+        s = ""
+        print("\n Dealer has the following cards:\n")
+        for c in self.hand:
+            s += self.card_names[c]
+        s += "for a total of \n" + str(self.get_total()) + "\n"
+
+
+def get_positive_int_up_to(prompt, max):
+    n = -1
+    while n < 0:
+        print("*" * 45)
+        c = input(prompt + "\n")
+        if c.isnumeric() and int(c) > 0 and int(c) <= max:
+            n = int(c)
+        elif c.isalpha():
+            print("Please enter a number")
+        elif c.isnumeric() and int(c) <= 0:
+            print("Please enter a number higher than 0")
+        elif c.isnumeric() and int(c) > max:
+            print("Your number is too high, max selection is " + str(max))
+        else:
+            print("Invalid input, please try again")
+    return n
+
 
 def initialize_players(number, money):
     players = []
     # add extra player to act as the dealer
-    #the dealer will be players[0]
+    # the dealer will be the first ([0]) object in the players list
     for x in range(number + 1):
         players.append(Player(money))
     return players
@@ -124,18 +159,46 @@ def initial_deal(players_list, deck):
     for p in players_list:
         p.get_card(deck)
 
-def mainloop():
+
+def check_for_blackjack(players):
+    for p in players:
+        if p.get_total() == 21:
+            p.blackjack = True
+
+
+def ask_player_moves(players, d):
+    assert players == Player
+    assert d == Deck
+    for i, p in enumerate(players):
+        prompt = "Player " + str(i) + ", what is your move? Choose 1 for stay, 2 to hit")
+        move = get_positive_int_up_to(prompt, 2)
+        while move != 1 and not p.bust and not p.blackjack:
+            if move == 2:
+                p.get_card(d)
+            p.bust_or_not()
+            p.check_blackjack()
+            print(p.return_status_string(i))
+            move = get_positive_int_up_to(prompt, 2)
+
+
+def payout_clear_hands(players):
+    pass
+
+
+def main_loop():
     shoe = Deck(1)
-    players = initialize_players(2, 1000)
+    players = initialize_players(4, 1000)
 
     initial_deal(players, shoe)
+    check_for_blackjack(players)
+    ask_player_moves(players, d)
+    players[0].dealer_moves(shoe)
+    payout_clear_hands(players)
 
     for i, p in enumerate(players):
         print(p.return_status_string(i))
 
 
-mainloop()
-#TODO implement app loop logic
+main_loop()
 #TODO implement strategies
-#TODO implement dealer strategy
 #TODO implement winning/losing and money transactions
