5f27fe2fa3adfe146085bdb2423db86b1351e4bc
Ben Jacobs <jacben13@gmail.com>
2014-09-12 20:55:05 -0700

    level 2 python challenge

---
+++ pythonchallenge.py
@@ -0,0 +1,8 @@
+__author__ = 'Ben'
+
+scramble = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb\
+ rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+decoded = ""
+for a in scramble:
+    decoded += chr(ord(a) + 2)
+print(decoded)
